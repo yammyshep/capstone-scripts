b359b3f3e041fd9082c61ea94617ee61b0fd58aa
YammyShep <jdbuelow@gmail.com>
2024-03-08 12:55:17 -0600

    make paths relative to repository root

--- subversion/lockman.py
+++ subversion/lockman.py
@@ -1,3 +1,8 @@
+# Subversion lock manager tool
+#
+# Lists locks currently held by users in an easy-to-read way
+
+import os.path
 import subprocess
 import xml.etree.cElementTree as ET
 
@@ -14,7 +19,7 @@ def get_locks(repo_path):
             lock_owner = lock.find('owner').text
             path = entry.get('path')
 
-            locks.setdefault(lock_owner, []).append(path)
+            locks.setdefault(lock_owner, []).append(os.path.relpath(path, repo_path))
 
     return locks
 
@@ -25,6 +30,7 @@ def print_locks(repo_path):
 
         for file in users[user]:
             print(f"    {file}")
+        print()
 
 
 print_locks("/home/yammyshep/repos/FA23-LandOfLights")
